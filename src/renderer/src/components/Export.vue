a98ceb9477f3fb952c350c83adc710dcfa11003f
Paul Ehrling <ghostlest330@gmail.com>
2024-05-29 18:28:52 +0200

    chore: Update npm dependencies and add Dockerode package

--- src/renderer/src/components/Export.vue
+++ src/renderer/src/components/Export.vue
@@ -1,15 +1,28 @@
 <template>
   <div>
     <p>Export image</p>
-    <button @click="getImages">Get images</button>
+    <div v-for="(image, index) in images" :key="index" style="display: flex; justify-content: space-between; align-items: center;">
+      <img :src="image.src" :alt="image.alt" />
+      <button @click="exportImage(image)">Export</button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+const images = ref([]);
+
 const getImages = () => {
   window.electron.send('getAllImages')
-  window.electron.receive('allImages', (images) => {
-    console.log(images)
+  window.electron.receive('allImages', (receivedImages) => {
+    images.value = receivedImages;
   })
+}
+
+onMounted(getImages);
+
+const exportImage = (image) => {
+  // Add your image export logic here
 }
 </script>
